f41490f674bb0ee4adb129c56f21c7beb3d0e1b8
zettyaffandi <zettyaffandi@gmail.com>
2022-04-05 12:15:09 +0800

    Latest update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,7 +59,7 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1fe09575daa1fee8e888bf70920cef4a",
 "/": "1fe09575daa1fee8e888bf70920cef4a",
-"main.dart.js": "2f4e65a7879490bea122cd143c03c891",
+"main.dart.js": "c1bef9808c2778f5ab7af75b84560008",
 "manifest.json": "affe8bb4f8178d1b3a465afba3cd4618",
 "version.json": "5428947d48f8b5d0fceb064d59bbfbbc"
 };
